7cad186776c1e22b5084c985e2e7f1e8c7869a71
Christoph Weygand <christophweygand@gmail.com>
2013-06-25 23:48:49 +0200

    created tests for luminous helper

---
+++ app/tests/library/luminous/HelperTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: waigie
+ * Date: 25.06.13
+ * Time: 22:37
+ * To change this template use File | Settings | File Templates.
+ */
+
+class HelperTest extends TestCase {
+
+    public function testOptionsEmpty() {
+
+        $luminous_mock = \Mockery::mock('LuminousInterface');
+        $luminous_mock->shouldReceive('scanners')->andReturn(array());
+
+        App::instance('Luminous', $luminous_mock);
+
+        $this->assertEquals(\cweygand\Nopaste\Luminous\LuminousViewHelper::options(), array());
+
+    }
+
+    public function testOptionsData() {
+
+        $luminous_mock = \Mockery::mock('LuminousInterface');
+        $luminous_mock->shouldReceive('scanners')->andReturn(array(
+            'C++' => array('c','cpp'),
+            'PHP' => array('php'),
+            'C#' => array('cs', 'csharp')
+        ));
+
+        App::instance('Luminous', $luminous_mock);
+
+        $this->assertEquals(\cweygand\Nopaste\Luminous\LuminousViewHelper::options(), array(
+            'c' => 'C++',
+            'php' => 'PHP',
+            'cs' => 'C#'
+        ));
+
+
+    }
+
+}
